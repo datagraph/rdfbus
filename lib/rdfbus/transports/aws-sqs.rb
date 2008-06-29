6d98355a205f2f4f35a9f0a57b59d8af301ed32c
Arto Bendiken <arto@bendiken.net>
2008-06-29 18:00:25 +0200

    Imported a basic Amazon SQS transport adapter.

--- lib/rdfbus/transports/aws-sqs.rb
+++ lib/rdfbus/transports/aws-sqs.rb
@@ -1,5 +1,29 @@
+require 'digest/sha1'
+require 'right_aws'
+
 module RDFbus::Transports
   class AmazonSQS < RDFbus::Transport
-    # TODO
+
+    def initialize(uri, options = {})
+      super
+      @conn = RightAws::SqsGen2.new(ENV['AMAZON_ACCESS_KEY_ID'], ENV['AMAZON_SECRET_ACCESS_KEY'])
+      @queue = @conn.queue(Digest::SHA1.hexdigest(uri.to_s), true)
+    end
+
+    def publish(payload)
+      @queue.send_message(payload.to_s)
+    end
+
+    def subscribe(interval = 2, timeout = nil, &block)
+      start = Time.now
+      loop do
+        if msg = @queue.receive
+          block.call(msg.body)
+          msg.delete
+        end
+        sleep interval
+        break if !timeout.nil? && (Time.now - start) >= timeout
+      end
+    end
   end
 end
