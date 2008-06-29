009662522c564061c9e74139fff363e55dccc7c9
Arto Bendiken <arto@bendiken.net>
2008-06-29 21:15:28 +0200

    Implemented initial API for transactions.

--- lib/rdfbus/transaction.rb
+++ lib/rdfbus/transaction.rb
@@ -1,4 +1,32 @@
 module RDFbus
   class Transaction
+    attr_reader :uuid
+    attr_reader :inserts
+    attr_reader :deletes
+
+    def self.parse(io)
+      self.new # TODO
+    end
+
+    def initialize(uuid = nil, inserts = [], deletes = [])
+      @uuid = uuid || `uuidgen`.chomp.downcase
+      @inserts, @deletes = inserts, deletes
+    end
+
+    def uri
+      "urn:uuid:#{uuid}"
+    end
+
+    def insert(*stmts)
+      @inserts += stmts
+    end
+
+    def delete(*stmts)
+      @deletes += stmts
+    end
+
+    def to_a
+      # TODO
+    end
   end
 end
