3cb5b2d3da0bbd2450ade81228ec2db53206e1e7
Arto Bendiken <arto@bendiken.net>
2010-02-09 05:27:48 +0100

    Implemented RDFbus::Transport.

--- lib/rdfbus/transport.rb
+++ lib/rdfbus/transport.rb
@@ -5,29 +5,33 @@ module RDFbus
   # @abstract
   class Transport
     ##
-    # @return [URI]
-    attr_reader :uri
+    # The transport endpoint's URL.
+    #
+    # @return [RDF::URI]
+    attr_reader :url
 
     ##
+    # Returns the implementation class for the given transport name.
+    #
     # @param  [Symbol] protocol
-    # @return [Transport]
+    # @return [Class]
     def self.for(protocol)
       case protocol.to_s.to_sym
-        when :amqp  then RDFbus::Transport::AMQP
-        when :aws   then RDFbus::Transport::AWS
-        when :stomp then RDFbus::Transport::Stomp
-        when :xmpp  then RDFbus::Transport::XMPP
+        when :amqp then RDFbus::AMQP
       end
     end
 
     ##
-    # @param  [URI] uri
+    # Open a connection to a transport endpoint.
+    #
+    # @param  [RDF::URI]               url
+    # @param  [Hash{Symbol => Object}] options
     # @yield  [transport]
-    # @yieldparam [Transport]
+    # @yieldparam [Transport] transport
     # @return [Transport]
-    def self.open(uri, &block)
+    def self.open(url, options = {}, &block)
       protocol  = :amqp # FIXME
-      transport = self.for(protocol).new(uri)
+      transport = self.for(protocol).new(url, options)
 
       if !block_given?
         transport
@@ -39,36 +43,55 @@ module RDFbus
     end
 
     ##
-    # @param  [URI] uri
-    def initialize(uri, options = {})
-      @uri = case uri
-        when RDF::URI then uri
-        else RDF::URI.new(uri.to_s)
+    # Initializes this transport.
+    #
+    # @param  [RDF::URI, #to_s]        url
+    # @param  [Hash{Symbol => Object}] options
+    def initialize(url, options = {})
+      @url = case url
+        when RDF::URI then url
+        else RDF::URI.new(url.to_s)
       end
       open
     end
 
     ##
+    # Opens a connection to the transport endpoint.
+    #
+    # @return [void]
     # @abstract
     def open() end
 
     ##
+    # Closes the connection to the transport endpoint.
+    #
+    # @return [void]
     # @abstract
     def close() end
 
     ##
-    # @raise [NotImplementedError] unless implemented in subclass
+    # Transmits a transaction over the transport connection.
+    #
+    # @param  [Transaction]            payload
+    # @param  [Hash{Symbol => Object}] options
+    # @return [void]
+    # @raise  [NotImplementedError] unless implemented in subclass
     # @abstract
-    def publish(payload)
-      raise NotImplementedError
+    def publish(payload, options = {})
+      raise NotImplementedError.new("#{self.class}#publish is not implemented yet")
     end
 
     ##
-    # @raise [NotImplementedError] unless implemented in subclass
+    # Receives transactions over the transport connection.
+    #
+    # @param  [Hash{Symbol => Object}] options
+    # @yield  [payload]
+    # @yieldparam [Transaction] payload
+    # @return [void]
+    # @raise  [NotImplementedError] unless implemented in subclass
     # @abstract
-    def subscribe(&block)
-      raise NotImplementedError
+    def subscribe(options = {}, &block)
+      raise NotImplementedError.new("#{self.class}#subscribe is not implemented yet")
     end
-
   end
 end
